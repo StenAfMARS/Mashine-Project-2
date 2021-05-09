2c9322d94040b3dab048374cc0343b9a28348fb7
mathias <mathias.baerentzen@gmail.com>
2021-05-09 16:00:21 +0200

    fixet punkt 1 2 4 5 6 7(mangler lidt) 8

--- Mashine Project 2/Mashine Project 2.c
+++ Mashine Project 2/Mashine Project 2.c
@@ -15,11 +15,15 @@ struct Node {
 
 struct Node* head;
 struct Node* tail;
-
+struct Node* Cards;
+void swap();
+void cutDeck();
+void safeToFile();
+void gameBegan();
 void push();
 void pop();
 void printList();
-
+int grid[7][8];
 
 void initializeGameScreen()
 {
@@ -45,12 +49,11 @@ void initializeGameScreen()
 	printf("Message: \n");
 	printf("Input >\n");
 }
-
-void generateGameBoard()
+void loadDeck()
 {
 	system("cls");
 
-	int grid[7][8];
+
 	char Hidden = "0";
 
 	// need generate card function
@@ -59,7 +62,71 @@ void generateGameBoard()
 	//first loop work with row
 	printf(" C1\tC2\tC3\tC4\tC5\tC6\tC7\n");
 	printf("\n");
-	struct Node* Cards = head;
+	
+	Cards = head;
+	//Generate grid space
+	bool endOfNode = false;
+	for (int j = 1; j <= 7; j++)
+	{
+		for (int k = 0; k < 7; k++)
+		{
+			if (k == 0)
+			{
+				
+
+			}
+			else
+			{
+				printf(" \[]\t", grid[j][k]);
+
+			}
+		}
+
+		if (j == 1)
+		{
+			printf("\t\[]\tF%d\n", j);
+		}
+		else
+		{
+			if (j == 3)
+			{
+				printf("\t\[]\tF%d\n", j - 1);
+			}
+			else
+			{
+				if (j == 5)
+				{
+					printf("\t\[]\tF%d\n", j - 2);
+				}
+				else
+				{
+					if (j == 7)
+					{
+						printf("\t\[]\tF%d\n", j - 3);
+					}
+					else
+					{
+						printf("\n");
+					}
+				}
+			}
+		}
+	}
+}
+void ShowDeck()
+{
+	system("cls");
+
+	
+	char Hidden = "0";
+
+	// need generate card function
+	char card = "C";
+
+	//first loop work with row
+	printf(" C1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+	printf("\n");
+	Cards = head;
 	//Generate grid space
 	bool endOfNode = false;
 	for (int j = 1; j <= 7; j++)
@@ -164,6 +231,10 @@ int main()
 	char name[20];
 	bool endOfGame = false;
 	int Input;
+	int cut;
+
+	int number;
+	int position;
 	while(endOfGame == false)
 	{
 		printf("type command");
@@ -174,7 +245,8 @@ int main()
 			// Load Deck
 		case 1: // LD = 1
 
-
+			head = NULL;
+			tail = NULL;
 			printf("Name File");
 			scanf("%s", name);
 			fp = fopen(name, "r");		// Open the file with 'read' option.	''
@@ -189,28 +261,139 @@ int main()
 				tail->data = strdup(tempArray);
 			}
 			fclose(fp);
-			printList();
-			printf("\n");
-			generateGameBoard();
-			generateGameBoard();
+			loadDeck();
 			break;
 
 			//show Deck
 		case 2: // SD = 2
+			ShowDeck();
 			break;
-		case 3: // End = 3
+		case 3: // Si = 2
+			printf("number of cut");
+			scanf("%d", &cut);
+			cutDeck(cut);
+			ShowDeck();
+			break;
+		case 4: // Sr = 2
+			swap();
+			ShowDeck();
+			break;
+		case 5: // SD = 2
+			safeToFile();
+			ShowDeck();
+			break;
+		case 6: // End = 6
 			endOfGame = true;
 			break;
+		case 7: // End = 6
+			gameBegan();
+			break;
+		case 11: // End = 3
+			printf("Name File\n");
+			printf("Name File\n");
+			printf("Name File\n");
+			printf("Name File\n");
+			break;
 		default:
-			printf("Unknown command");
+			printf("Unknown command\n");
 		}
 	}
 
 
 	return 0;
 }
-//Doubly Linked List
+void cutDeck(int cut) {
+	struct Node* node1 = head;
+	struct Node* node2 = head;
+	for (int i = 0; i <= cut; i++) {
+		node2->data = node1->data;
+		node1 = node1->next;
+		node2 = node2->next;
+	}
+	/*
+	while (node1->next != NULL)
+	{
+		node1 = node1->next;
+		if (node1 == NULL) {
+			node1 = head;
+		}
+		for (int i = rand() % 3; i >= 0; i--) {
+			node2 = node2->next;
+			if (node2 == NULL) {
+				node2 = head;
 
+			}
+		}
+		char* placeHolder = node1->data;
+		node1->data = node2->data;
+		node2->data = placeHolder;
+
+	}
+	*/
+}
+//Doubly Linked List
+void swap() {
+	struct Node* node1 = head;
+	struct Node* node2 = head;
+
+	for (int j = 0; j < 10;) {
+		node1 = node1->next;
+		if (node1 == NULL) {
+			node1 = head;
+			j++;
+		}
+		for (int i = rand() % 3; i >= 0; i--) {
+			node2 = node2->next;
+			if (node2== NULL) {
+				node2 = head;
+
+			}
+		}
+		char*placeHolder = node1->data;
+		node1->data = node2->data;
+		node2->data = placeHolder;
+		
+		
+	}
+	
+}
+void safeToFile() {
+	FILE* fp;
+	int i;
+	char name[50];
+	printf("file name: ");
+	scanf("%s", name);
+
+	fp = fopen(name, "w");
+	Cards = head;
+	while (Cards->next != NULL)
+	{
+		fprintf(fp, "\%s\n", Cards->data);
+		Cards = Cards->next;
+	}
+	fprintf(fp, "\%s\n", Cards->data);
+		/* close the file*/
+	fclose(fp);
+	}
+
+void gameBegan() {
+	bool gameStarted = true;
+		while(gameStarted == true)
+		{
+			int Input2;
+			printf("type command");
+			scanf("%d", &Input2);
+
+			switch (Input2) {
+			case 8:
+				gameStarted = false;
+				break;
+			default:
+				printf("Unknown command\n");
+			}
+			
+		}
+}
 void push(){
 	    
     if(head != NULL){
