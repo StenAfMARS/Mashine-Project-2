3d3ced0ec8607699bce55d648de685ef57916200
mathias <mathias.baerentzen@gmail.com>
2021-05-09 11:43:25 +0200

    setup ld

--- Mashine Project 2/Mashine Project 2.c
+++ Mashine Project 2/Mashine Project 2.c
@@ -2,8 +2,23 @@
 // ReSharper disable CppInconsistentNaming
 
 #include "Mashine Project 2.h"
-
+#include <stdio.h>
+#include <stdlib.h> // For exit() function
+#include <assert.h>
+#include <string.h>
 char lastComment[24];
+struct Node {
+	char* data;
+	struct Node* next; // Pointer to next node in DLL
+	struct Node* prev; // Pointer to previous node in DLL
+};
+
+struct Node* head;
+struct Node* tail;
+
+void push();
+void pop();
+void printList();
 
 
 void initializeGameScreen()
@@ -44,19 +59,54 @@ void generateGameBoard()
 	//first loop work with row
 	printf(" C1\tC2\tC3\tC4\tC5\tC6\tC7\n");
 	printf("\n");
-
+	struct Node* Cards = head;
 	//Generate grid space
+	bool endOfNode = false;
 	for (int j = 1; j <= 7; j++)
 	{
 		for (int k = 0; k < 7; k++)
 		{
 			if (k == 0)
 			{
-				printf(" \[]\t", grid[j][k]);
+				if (Cards->next == NULL) { // reached the last node, so stop iterating
+					if (endOfNode == false) {
+						printf(" \%s\t", Cards->data);
+						endOfNode = true;
+					}
+					else
+					{
+						printf(" \[]\t", grid[j][k]);
+					}
+					
+					
+				}
+				else {
+					printf(" \%s\t", Cards->data);
+					Cards = Cards->next;
+				}
+				
+
 			}
 			else
 			{
-				printf("\[]\t", grid[j][k]);
+				if (Cards->next == NULL) { // reached the last node, so stop iterating
+					if (endOfNode == false) {
+						printf(" \%s\t", Cards->data);
+						endOfNode = true;
+					}
+					else
+					{
+						printf(" \[]\t", grid[j][k]);
+					}
+
+
+				}
+				else {
+					printf(" \%s\t", Cards->data);
+					Cards = Cards->next;
+					
+				}
+
 			}
 		}
 
@@ -108,24 +158,96 @@ int main()
 {
 	//Initialize game screen
 	//initializeGameScreen();
-	generateGameBoard();
-
-	char Input = "";
-
+	
+	char tempArray[20];
+	FILE* fp;
+	bool endOfGame = 
+	printf("type command");
+	int Input = scanf("%d", &Input);;
+	
 	switch (Input)
 	{
 		// Load Deck
-	case 'LD':
+	case 1: // LD = 1
+		
+
+		printf("Name File");
+		fp = fopen("testData.txt", "r");		// Open the file with 'read' option.	''
+		if (fp == NULL) { exit(-1); }
+		while (!feof(fp)) {
+
+			fgets(tempArray, 20, fp);
+			if (tempArray[strlen(tempArray) - 1] == '\n') {
+				tempArray[strlen(tempArray) - 1] = 0;
+			}
+			push();
+			tail->data = strdup(tempArray);
+		}
+		fclose(fp);
+		printList();
+		printf("\n");
+		generateGameBoard();
 		generateGameBoard();
 		break;
 
 		//show Deck
-	case 'SD':
+	case 2: // SD = 2
 		break;
-
+	case 3: // End = 3
+		break;
 	default:
 		printf("Unknown command");
 	}
 
 	return 0;
+}
+//Doubly Linked List
+
+void push(){
+	    
+    if(head != NULL){
+        struct Node* temp1 = malloc(sizeof(struct Node));
+        temp1->next = NULL; 
+        temp1->prev = tail;
+        tail->next = temp1;
+        tail = temp1;
+    } else { //list is empty
+        head = malloc(sizeof(struct Node));	// setting up space in the memory for the 1st node.
+        head->prev = NULL;
+        head->next = NULL;
+        tail = head;
+        printf("Node specific addr: %p\n", head);
+    }
+
+} 
+
+void pop(){
+    if(tail!=NULL){
+        struct Node* temp1;
+        if(tail->prev != NULL){
+            temp1 = tail;
+            tail = tail->prev;
+            free(temp1);
+            tail->next = NULL;
+        }
+    } else { printf("List is empty.\n"); }
+}
+
+/* Use this function to print out the current contents of memory. */
+void printList(){
+	
+	printf("------------------------------------"	
+		   "\n------------------------------------"
+		   "\nThis function will print info from all the nodes.\n");
+	struct Node* Derpina = head;
+	int derp =1;
+	while (1==1){
+		printf("Node data : %s\n", Derpina->data);
+		if(Derpina->next == NULL){ // reached the last node, so stop iterating
+			break;	
+		} else {
+			Derpina = Derpina->next;
+		}	
+	}	
+	return;
 }
